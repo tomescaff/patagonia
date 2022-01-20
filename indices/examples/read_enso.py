b1b1d9aa8f956b21a70c4b3cc87982acb947f0d7
Tomás Carrasco Escaff <tomescaff@gmail.com>
2022-01-20 01:06:49 -0300

    creating example script for reading enso indices

---
+++ indices/examples/read_enso.py
@@ -0,0 +1,14 @@
+import sys
+
+# append the path of the parent directory
+sys.path.append("..")
+
+from readers import enso
+
+nino12 = enso.nino12_index()
+nino34 = enso.nino34_index()
+nino4 = enso.nino4_index()
+oni = enso.oni_index()
+tni = enso.tni_index()
+ep = enso.ep_nino_index()
+cp = enso.cp_nino_index()
