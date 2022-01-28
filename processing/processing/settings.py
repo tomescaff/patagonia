6dc608a5343cd5f4b5f75b7b9187eba5aa424887
Tomás Carrasco Escaff <tomescaff@gmail.com>
2022-01-28 17:34:44 -0300

    adding settings for Era-Interim and RegCM basedirs

--- processing/processing/settings.py
+++ processing/processing/settings.py
@@ -6,3 +6,6 @@ SPI_MODEL_RESULTS_ROOT = '/Volumes/TOSHIBA_EXT/Documentos/datasets/climate/RegCM
 
 NPI_MODEL_RESULT_FILE = 'mb_mon_iceparm3_65LR_05PG_9.5scale_-6offset_100prfactor_0.0toff_p00umb_npi.nc'
 SPI_MODEL_RESULT_FILE = 'mb_mon_iceparm3_65LR_05PG_9.5scale_21offset_100prfactor_0.0toff_p00umb_spi.nc'
+
+REGCM_MONTH_ROOT  = '/Users/Tom/tmp2/mount/tcarrasc/datasets/climate/RegCM4/alldom_latlongrid/month/'
+ERAINT_MONTH_ROOT = '/Users/Tom/tmp2/mount/tcarrasc/datasets/climate/ERA_INTERIM/month/'
