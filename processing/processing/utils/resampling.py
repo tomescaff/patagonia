668a02f592356e9a5e1e11bf4ade371b009f5603
Tomás Carrasco Escaff <tomescaff@gmail.com>
2022-02-03 14:20:30 -0300

    refactoring resampling methods

--- processing/processing/utils/resampling.py
+++ processing/processing/utils/resampling.py
@@ -17,10 +17,10 @@ def ndays_per_month(init_date = '1980-04-01', end_date = '2015-03-31'):
 
 # transform monthly mean timeseries in [·/day] to monthly acc timeseries in [·/month]
 # use data from 1980-04 to 2015-03
-def to_monthly_acc_values(da_monthly):
+def to_monthly_acc_values(da_monthly, init_date = '1980-04-01', end_date = '2015-03-31'):
 
     # select time period
-    da_monthly = da_monthly.sel(time=slice('1980-04-01', '2015-03-31'))
+    da_monthly = da_monthly.sel(time=slice(init_date, end_date))
     
     # get numbers of days per month
     ndays_month = ndays_per_month()
@@ -37,16 +37,15 @@ def to_yearly_acc_values(da_monthly):
 # transform monthly mean timeseries in [·/day] to yearly mean timeseries in [·/year]
 # use data from 1980-04 to 2015-03
 def to_yearly_mean_values(da_monthly):
-    # select time period
-    da_monthly = da_monthly.sel(time=slice('1980-04-01', '2015-03-31'))
+    # to monthly acc timeseries in [·/month]
+    da_monthly_acc = to_monthly_acc_values(da_monthly)
     
     # compute numbers of days per month in xarray
-    dr = pd.date_range('1980-04-01','2015-03-31')
-    one_per_day = xr.DataArray( np.ones((dr.shape[0],)), coords=[dr], dims=['time'])
-    ndays_per_month = one_per_day.resample(time='1MS').sum().sel(time=slice('1980-04-01', '2015-03-31'))
+    ndays_month = ndays_per_month()
     
-    # compute yearly mean serie
-    da_monthly = da_monthly*ndays_per_month 
-    yearly_data_acc = da_monthly.resample(time='12MS').sum('time',skipna=False)
-    ndays_per_year = ndays_per_month.resample(time='12MS').sum('time',skipna=False)
+    # compute yearly acc serie
+    yearly_data_acc = da_monthly_acc.resample(time='12MS').sum('time',skipna=False)
+    ndays_per_year = ndays_month.resample(time='12MS').sum('time',skipna=False)
+
+    # return yearly mean serie
     return yearly_data_acc/ndays_per_year
