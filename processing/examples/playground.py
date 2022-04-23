4bcb1ad38e87a58db6ea60bf36bc01a76ed930a1
Tomás Carrasco Escaff <tomescaff@gmail.com>
2022-04-23 15:19:42 -0400

    Updating playground

--- processing/examples/playground.py
+++ processing/examples/playground.py
@@ -25,15 +25,36 @@ sam_data = sam_data.sel(time=slice('1980-04', '2015-03'))
 mb_data = mon_data['mb']
 pr_data = mon_data['pr']
 
-mb_summer = resampling.to_summer_acc_values(mb_data)
-pr_summer = resampling.to_summer_acc_values(pr_data)
+# mb_summer = resampling.to_summer_acc_values(mb_data)
+# pr_summer = resampling.to_summer_acc_values(pr_data)
 
-print(stats.pearsonr(mb_summer, pr_summer))
+# print(stats.pearsonr(mb_summer, pr_summer))
 
-mb_winter = resampling.to_winter_acc_values(mb_data)
-pr_winter = resampling.to_winter_acc_values(pr_data)
+# mb_winter = resampling.to_winter_acc_values(mb_data)
+# pr_winter = resampling.to_winter_acc_values(pr_data)
 
-print(stats.pearsonr(mb_winter, pr_winter))
+# print(stats.pearsonr(mb_winter, pr_winter))
 
+mb_annual = resampling.to_yearly_acc_values(mon_data['mb'], from_monthly_acc=True)
+pr_annual = resampling.to_yearly_acc_values(mon_data['pr'], from_monthly_acc=True)
+acc_annual = resampling.to_yearly_acc_values(mon_data['acc'], from_monthly_acc=True)
+abl_annual = resampling.to_yearly_acc_values(mon_data['abl'], from_monthly_acc=True)
+tas_annual = resampling.to_yearly_mean_values(mon_data['tas'])
+rsds_annual = resampling.to_yearly_mean_values(mon_data['rsds'])
+
+mb_winter = resampling.to_winter_acc_values(mon_data['mb'], from_monthly_acc=True)
+pr_winter = resampling.to_winter_acc_values(mon_data['pr'], from_monthly_acc=True)
+acc_winter = resampling.to_winter_acc_values(mon_data['acc'], from_monthly_acc=True)
+abl_winter = resampling.to_winter_acc_values(mon_data['abl'], from_monthly_acc=True)
+tas_winter = resampling.to_winter_mean_values(mon_data['tas'])
+rsds_winter = resampling.to_winter_mean_values(mon_data['rsds'])
+
+mb_summer = resampling.to_summer_acc_values(mon_data['mb'], from_monthly_acc=True)
+pr_summer = resampling.to_summer_acc_values(mon_data['pr'], from_monthly_acc=True)
+acc_summer = resampling.to_summer_acc_values(mon_data['acc'], from_monthly_acc=True)
+abl_summer = resampling.to_summer_acc_values(mon_data['abl'], from_monthly_acc=True)
+tas_summer = resampling.to_summer_mean_values(mon_data['tas'])
+rsds_summer = resampling.to_summer_mean_values(mon_data['rsds'])
     
     
+f=lambda x: (x.mean(), x.std())
